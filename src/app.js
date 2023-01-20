672dbe0d0f18841604c30c87dee0b998488f045e
paul-war <pablodanielguerra@gmail.com>
2023-01-20 16:45:04 +0000

    v4

--- src/app.js
+++ src/app.js
@@ -19,32 +19,38 @@ const numbers = [
   "Ace"
 ];
 
-function generateRandomCards() {
+var suit1 = document.querySelector("#firstsuit");
+var suit2 = document.querySelector("#secondsuit");
+var number = document.querySelector("h2");
+var btn = document.querySelector("#button");
+
+function generateRandomCard() {
   //generando un número aleatorio para seleccionar un suit
   var randomSuit = suits[Math.floor(Math.random() * suits.length)];
   //generando un número aleatorio para seleccionar un number
   var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
-  // mostrando en consola el resultado
-  console.log("La carta generada es: " + randomNumber + " de " + randomSuit);
+  //Modifico el contenido del h1 por el número generado
+  number.innerHTML = randomNumber;
+
+  //modifico el color del "palo/traje"
+  if (randomSuit === "Hearts") {
+    suit1.style.color = "red";
+    suit2.style.color = "red";
+    suit1.innerHTML = "♥";
+    suit2.innerHTML = "♥";
+  } else if (randomSuit === "Diamonds") {
+    suit1.style.color = "red";
+    suit2.style.color = "red";
+    suit1.innerHTML = "♦";
+    suit2.innerHTML = "♦";
+  } else if (randomSuit === "Spades") {
+    suit1.innerHTML = "♠";
+    suit2.innerHTML = "♠ ";
+  } else {
+    suit1.innerHTML = "♣";
+    suit2.innerHTML = "♣ ";
+  }
 }
-window.onload = generateRandomCards();
 
-/*
-function generateCard() {
-  // Get a random suit and value
-  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-  const randomValue = values[Math.floor(Math.random() * values.length)];
-
-  // Create a new `div` element for the card
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  // Add the suit and value as text inside the card
-  card.innerHTML = `${randomValue} of ${randomSuit}`;
-
-  // Add the card to the card container
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.appendChild(card);
-}
-*/
+window.onload = generateRandomCard();
